14767a65db8d63e0c716764468e2545c1b70e37b
GuruCoyote <gc@testbild.tv>
2022-05-17 11:48:31 +0200

    * used the cmdline batch code formater Forater.exe

--- Karten.dpr
+++ Karten.dpr
@@ -1,110 +1,120 @@
 ﻿program Karten;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
-	System.SysUtils,
-	Windows,
-	Classes,
-	Spiel in 'Spiel.pas';
+  System.SysUtils,
+  Windows,
+  Classes,
+  Spiel in 'Spiel.pas';
 
 var
-	lInput, lLastInput: String;
-	lCmdList: TStrings;
-	stapel: TStapel;
+  lInput, lLastInput: String;
+  lCmdList: TStrings;
+  stapel: TStapel;
 
-	procedure laden(deckName: String);
+procedure laden(deckName: String);
 begin
-	stapel := TStapel.create(deckName);
-	stapel.mischen;
-	Writeln(IntToStr(stapel.FKarten.count) + 'Karten geladen');
+  stapel := TStapel.create(deckName);
+  stapel.mischen;
+  Writeln(IntToStr(stapel.FKarten.count) + 'Karten geladen');
 end;
 
 procedure ziehen(anz: Integer);
-var 
-k: TKarte;
-i: Integer;
+var
+  k: TKarte;
+  i: Integer;
 begin
-	k := TKarte.create('', -1, '', '');
-	try
-		for i := 1 to anz  do
-		begin
-			k := stapel.zieheKarte;
-			WriteLn(IntToStr(i) + ' gezogen:');
-			WriteLn(Utf8String(k.aufdecken));
-			if k.FZahl = 0 then
-				break;
-			end; // end loop
-		finally
-			k.free;
-		end; // try
-	end;
-
-	// main
-begin
-	SetConsoleOutputCP(CP_UTF8);
-	SetTextCodePage(Output, CP_UTF8);
-	try
-		// handle command line params
-		if FindCmdLineSwitch( 'l', lInput) then
-		begin
-			laden(lInput);
-			if FindCmdLineSwitch( 'z', lInput) then
-			begin
-				try
-					ziehen(StrToInt(lInput));
-				except
-					ziehen(1);
-				end;
-			end;
-		end;
-
-// handle interactive user input
-	lCmdList := TStringList.create;
-	try
-		lLastInput := '.';
-		repeat
-			ReadLn(lInput);
-			if lInput.length = 0 then 
-	lInput := lLastInput;
-
-	lCmdList.clear;
-ExtractStrings([' '], [], PChar(lInput), lCmdList);
-// var c: Integer;
-// for c := 0 to lCmdList.count-1 do
-// 	begin
-// 	WriteLn(lCmdList[c]);
-// end;
-
-			case lCmdList[0][1] of
-			'l': begin
-				if lCmdList.count > 1 then
-					laden(lCmdList[1] )
-				else 
-				laden('test');
-			end;
-			'z': begin
-				if lCmdList.count > 1 then
-					ziehen(StrToInt(lCmdList[1]))
-				else
-					ziehen(1);
-			end;
-			'm': laden(stapel.FName);
-			'q': lInput := '.';// end this loop
-			'.': WriteLn('Und Tschüß');
-		else  WriteLn('unbekannter Befehl ' + lInput);
-	end;
-	lLastInput := lInput;
-until lInput = '.';
-
+  k := TKarte.create('', -1, '', '');
+  try
+    for i := 1 to anz do
+    begin
+      k := stapel.zieheKarte;
+      Writeln(IntToStr(i) + ' gezogen:');
+      Writeln(Utf8String(k.aufdecken));
+      if k.FZahl = 0 then
+        break;
+    end; // end loop
   finally
-	  lCmdList.free;
-stapel.free;
+    k.free;
+  end; // try
 end;
+
+// main
+begin
+  SetConsoleOutputCP(CP_UTF8);
+  SetTextCodePage(Output, CP_UTF8);
+  try // outer app try block
+    // handle command line params
+    if FindCmdLineSwitch('l', lInput) then
+    begin
+      laden(lInput);
+      if FindCmdLineSwitch('z', lInput) then
+      begin
+        try
+          ziehen(StrToInt(lInput));
+        except
+          ziehen(1);
+        end;
+      end; // end z param
+      // if user didn't request interactive mode, exit
+      if not FindCmdLineSwitch('i', lInput) then
+        exit
+      else
+        lLastInput := 'z';
+    end;
+
+    // handle interactive user input
+    lCmdList := TStringList.create;
+    try
+      repeat
+        ReadLn(lInput);
+        if lInput.length = 0 then
+          lInput := lLastInput;
+
+        lCmdList.clear;
+        ExtractStrings([' '], [], PChar(lInput), lCmdList);
+        // var c: Integer;
+        // for c := 0 to lCmdList.count-1 do
+        // begin
+        // WriteLn(lCmdList[c]);
+        // end;
+
+        case lCmdList[0][1] of
+          'l':
+            begin
+              if lCmdList.count > 1 then
+                laden(lCmdList[1])
+              else
+                laden('test');
+            end;
+          'z':
+            begin
+              if lCmdList.count > 1 then
+                ziehen(StrToInt(lCmdList[1]))
+              else
+                ziehen(1);
+            end;
+          'm':
+            laden(stapel.FName);
+          'q':
+            lInput := '.'; // end this loop
+          '.':
+            Writeln('Und Tschüß');
+        else
+          Writeln('unbekannter Befehl ' + lInput);
+        end;
+        lLastInput := lInput;
+      until lInput = '.';
+
+    finally
+      lCmdList.free;
+      stapel.free;
+    end;
   except
-	  on E: Exception do
-		  Writeln(E.ClassName, ': ', E.Message);
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
   end;
+
 end.
--- Spiel.pas
+++ Spiel.pas
@@ -1,130 +1,132 @@
-  unit Spiel;
+unit Spiel;
 
+interface
 
-  interface
 uses
-	Classes, // Unit containing the tstringlist 
-	Generics.Collections;
+  Classes, // Unit containing the tstringlist
+  Generics.Collections;
 
 type
-		 TKarte = class
-			 FName: String;
-			 FZahl: Integer;
-			 FHell: String;
-			 FDunkel: String;
-			 constructor create(name: String; zahl: Integer; hell: String; dunkel: String);
-			 function aufdecken: String; 
-		 end;
+  TKarte = class
+    FName: String;
+    FZahl: Integer;
+    FHell: String;
+    FDunkel: String;
+    constructor create(name: String; zahl: Integer; hell: String;
+      dunkel: String);
+    function aufdecken: String;
+  end;
 
-		 type
-			 TStapel = class
-				 FName: String;
-	FKarten: TObjectList<TKarte>;
-constructor create(deckName: String);
-	function zieheKarte(): TKarte;
-	procedure  mischen;
-			 end;
+type
+  TStapel = class
+    FName: String;
+    FKarten: TObjectList<TKarte>;
+    constructor create(deckName: String);
+    function zieheKarte(): TKarte;
+    procedure mischen;
+  end;
 
-  type
-	  TSpiel = class
+type
+  TSpiel = class
 
-		  FName: String;
+    FName: String;
 
-		  TZiehStapel: TStapel;
-		  FAblageStapel: TStapel;
+    TZiehStapel: TStapel;
+    FAblageStapel: TStapel;
 
-
-end;
+  end;
 
 implementation
-uses 
-IniFiles,
-SysUtils,
-System.Generics.Defaults;
 
-			 constructor TKarte.create(name: String; zahl: Integer; hell: String; dunkel: String);
-begin
-	inherited Create;
-	FName := name;
-				 FZahl := zahl;
-				 FHell := hell;
-				 FDunkel := dunkel;
-			 end;
+uses
+  IniFiles,
+  SysUtils,
+  System.Generics.Defaults;
 
-function TKarte.aufdecken: String; 
-var s: String;
+constructor TKarte.create(name: String; zahl: Integer; hell: String;
+  dunkel: String);
 begin
-	randomize;
-	s := FName;
-case random(2)  of
-0: s := s + sLineBreak + 'Dunkel: ' + FDunkel;
-1: s:= s + sLineBreak + 'Hell: ' + FHell;
-		else Writeln('huh?');
+  inherited create;
+  FName := name;
+  FZahl := zahl;
+  FHell := hell;
+  FDunkel := dunkel;
 end;
-	Result := s;
+
+function TKarte.aufdecken: String;
+var
+  s: String;
+begin
+  randomize;
+  s := FName;
+  case random(2) of
+    0:
+      s := s + sLineBreak + 'Dunkel: ' + FDunkel;
+    1:
+      s := s + sLineBreak + 'Hell: ' + FHell;
+  else
+    Writeln('huh?');
+  end;
+  Result := s;
 end;
 
 function deckDatei(deckName: String): String;
 begin
-	  Result :=  GetCurrentDir + PathDelim + deckName + '.deck';
+  Result := GetCurrentDir + PathDelim + deckName + '.deck';
 end;
 
 constructor TStapel.create(deckName: String);
-var 
+var
   ini: TmemIniFile;
   filename: String;
   cards: TStringList;
   i: Integer;
   karte: TKarte;
 begin
-	inherited Create;
-	// set our name
-	FName := deckName;
-	// create our card list
-	FKarten := TObjectList<TKarte>.create(true);
+  inherited create;
+  // set our name
+  FName := deckName;
+  // create our card list
+  FKarten := TObjectList<TKarte>.create(true);
 
-	filename := deckDatei(deckName);
+  filename := deckDatei(deckName);
   // TODO: first check if deck file with given name exists
-    ini := TMemIniFile.Create(filename, TEncoding.UTF8);
-	cards := TStringList.create;
+  ini := TmemIniFile.create(filename, TEncoding.UTF8);
+  cards := TStringList.create;
 
-    try
-	    // ini.WriteString('Meine Straße', 'Haus', 'tröt');
-	     ini.readSections(cards);
-	     for i := 0 to cards.count -1 do
-		     begin
-		     karte := TKarte.create(
-		     cards[i],
-		     i+1,
-		     ini.ReadString(cards[i], 'hell', 'h'),
-		     ini.ReadString(cards[i], 'dunkel', 'd'));
-	FKarten.add(karte);
-		     end;
+  try
+    // ini.WriteString('Meine Straße', 'Haus', 'tröt');
+    ini.readSections(cards);
+    for i := 0 to cards.count - 1 do
+    begin
+      karte := TKarte.create(cards[i], i + 1, ini.ReadString(cards[i], 'hell',
+        'h'), ini.ReadString(cards[i], 'dunkel', 'd'));
+      FKarten.add(karte);
+    end;
 
-		        finally
-				    ini.Free;
-				    cards.free;
-				      end;
-			      end;
-
-	function TStapel.zieheKarte(): TKarte;
-begin
-	try
-		Result  := FKarten.extract( FKarten[0]);
-	Except
-		Result := TKarte.create('Keine Karten im Stapel', 0, 'bitte neu mischen', 'bitte neu mischen');
-	end;
+  finally
+    ini.Free;
+    cards.Free;
+  end;
 end;
 
-procedure  TStapel.mischen;
+function TStapel.zieheKarte(): TKarte;
 begin
-	FKarten.Sort(TComparer<TKarte>.Construct(
-		function (const L, R: TKarte): integer
+  try
+    Result := FKarten.extract(FKarten[0]);
+  Except
+    Result := TKarte.create('Keine Karten im Stapel', 0, 'bitte neu mischen',
+      'bitte neu mischen');
+  end;
+end;
+
+procedure TStapel.mischen;
 begin
-	randomize;
-	Result:=-1 + Random(3);
-end
-));
-	end;
+  FKarten.Sort(TComparer<TKarte>.Construct(function(const L, R: TKarte): Integer
+    begin
+      randomize;
+      Result := -1 + random(3);
+    end));
+end;
 
 end.
